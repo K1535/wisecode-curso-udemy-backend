daeaaecedad119d45de6fc2cbab0486314eb92b4
K1535 <167162183+K1535@users.noreply.github.com>
2025-09-30 22:59:07 -0600

    creacion de las tablas/video-60

---
+++ database/migrations/2025_10_01_045543_create_providers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('providers', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('full_name', 250);
+            $table->string('ruc', 150);
+            $table->string('email', 250)->nullable();
+            $table->string('phone', 50)->nullable();
+            $table->string('address', 250)->nullable();
+            $table->string('imagen', 250);
+            $table->unsignedTinyInteger('state')->default(1);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('providers');
+    }
+};
